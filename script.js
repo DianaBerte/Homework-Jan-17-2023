91e8cb35804397c798c2321479258b599c811b9d
DianaBerte <dianaberte.go@gmail.com>
2023-01-17 16:35:17 +0100

    committing

--- script.js
+++ script.js
@@ -13,3 +13,35 @@ const getImages = (query) => {
 }
 
 getImages("dogs")
+
+
+
+// const svg = document.querySelectorAll("bd-placeholder-img");
+// const newH2 = document.createElement("h2");
+
+// function deleteImages () {
+
+// if (svg === "bd-placeholder-img")
+// {svg.replaceWith(newH2)}
+
+// }
+
+// deleteImages()
+
+
+
+// for (let i = 0; i < svg.length; i++) {
+//     // console.log(svg[i])
+//     let whatever = svg[i];
+//     whatever.delete;
+    
+// }
+
+// const img = document.createElement("img");
+// const cardNode = document.getElementsByClassName("card")[1];
+// cardNode.appendChild("img")
+
+// svg.replaceWith(img)
+
+// svg.remove()
+
