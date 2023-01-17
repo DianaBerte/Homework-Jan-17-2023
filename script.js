a25f8c0e1d5f42dad5abc4de10a90436383e432a
DianaBerte <dianaberte.go@gmail.com>
2023-01-17 15:18:24 +0100

    fetching

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-const loadImagesBtn = () => {
-   fetch('https://api.pexels.com/v1/search?query=563492ad6f917000010000017b1b51018c0c46df9be0b99163b943be', {
-   method: "GET",     
-   headers: {
-            Authorization: "Bearer 563492ad6f917000010000017b1b51018c0c46df9be0b99163b943be"
-        }
-   
-    }).then((response)=> {
-        return response.json()
-    }).then((data)=> {
-        console.log(data)
-    })
-    .catch(err => console.error(err))
+const options = {
+    method: "GET",
+    headers: {
+        Authorization: "Bearer 563492ad6f917000010000017b1b51018c0c46df9be0b99163b943be"
+    }
 }
+
+const getImages = (query) => {
+    fetch(`https://api.pexels.com/v1/search?query= ${query}`, options)
+
+.then((response) => response.json())
+.then((data) => {console.log(data)})
+}
+
+getImages("dogs")
